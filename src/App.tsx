6195cab456fe5431238aba4c732976ed76d844e3
FELIPE BECKFORD SISIS <49255481@ort>
2026-06-08 11:56:45 -0300

    sidebar componente hecho falta todo css

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import { Route, Routes } from "react-router-dom";
-import HomePage from "./components/HomePage";
+import HomePage from "./HomePage";
 import PatientHome from "./components/mobile/patientHome";
 import DoctorHome from "./components/web/doctorHome";
 
